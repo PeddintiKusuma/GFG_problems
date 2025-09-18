958b0ff1ec2414c3a3e44de55438000a05c51a43
PeddintiKusuma <peddintikusuma@gmail.com>
2025-09-18 15:15:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Shortest path from 1 to n/shortest-path-from-1-to-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minStep(int n) {
+        // code here
+        int steps=0;
+        while(n>1){
+            if(n%3==0){
+                n=n/3;
+            }
+            else{
+                n=n-1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
