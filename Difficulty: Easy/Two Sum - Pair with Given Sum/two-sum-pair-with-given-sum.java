947335baaf271af856efdda6ae597099fe4d684f
PeddintiKusuma <peddintikusuma@gmail.com>
2025-12-23 10:18:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.java
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.java
@@ -1,15 +1,15 @@
 class Solution {
     boolean twoSum(int arr[], int target) {
         // code here
-        Map<Integer,Integer> map= new HashMap<>();
+        Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<arr.length;i++){
             int diff=target-arr[i];
             if(map.containsKey(diff)){
                 return true;
             }
             map.put(arr[i],i);
-            
         }
         return false;
     }
 }
+
