35c863e1ba751b18e324af7d3955d4f47cfc5475
PeddintiKusuma <peddintikusuma@gmail.com>
2026-02-08 19:17:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Winner of an election/winner-of-an-election.py
+++ Difficulty: Easy/Winner of an election/winner-of-an-election.py
@@ -1,11 +1,20 @@
+#User function Template for python3
+
 class Solution:
-    def winner(self, arr):
+    
+    #Complete this function
+    
+    #Function to return the name of candidate that received maximum votes.
+    def winner(self,arr,n):
+        # Your code here
+        # return the name of the winning candidate and the votes he recieved
         votes = {}
 
         for name in arr:
             votes[name] = votes.get(name, 0) + 1
 
         max_votes = 0
+        
         winner_name = None
 
         for name, count in votes.items():
@@ -19,3 +28,4 @@ class Solution:
                 winner_name = name
 
         return [winner_name, str(max_votes)]
+
