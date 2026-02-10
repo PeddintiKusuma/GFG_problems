fcd9e94969acd0af23c2249ccf5970d43a62a77e
PeddintiKusuma <peddintikusuma@gmail.com>
2026-02-10 19:59:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Subset/array-subset.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+    #Function to check if a is a subset of b.
+    def isSubset(self, a, b):
+        # Your code here
+        a.sort()
+        b.sort()
+        i,j=0,0
+        while i<len(b) and j<len(a):
+            if b[i]==a[j]:
+                i=i+1
+                j=j+1
+            else:
+                j=j+1
+                
+        if i==len(b):
+            return True
+        else:
+            return False
+            
+    
+    
+    
+    
